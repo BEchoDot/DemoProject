ae2402fbf7a07a06c9dcd58a155ef1c2a78eb43a
namnk <nkn6294@live.co.uk>
2021-03-04 08:58:07 +0700

    update script file

---
+++ scripts/before_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# OPTIONAL Install apt addons
+# OPTIONAL Install cache components
+# before_install
+# install
+# before_script
+# script
+# OPTIONAL before_cache (if and only if caching is effective)
+# after_success or after_failure
+# OPTIONAL before_deploy (if and only if deployment is active)
+# OPTIONAL deploy
+# OPTIONAL after_deploy (if and only if deployment is active)
+# after_script
+echo 'Run test'
